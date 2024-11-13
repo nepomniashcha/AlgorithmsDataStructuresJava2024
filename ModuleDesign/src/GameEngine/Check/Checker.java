cf8e2d16b2d3ff7f9406bbf858f7ed6e9f6fe2f1
nepomniashcha <marina.a.n555@gmail.com>
2024-11-13 08:39:24 +0200

    .

--- ModuleDesign/src/GameEngine/Check/Checker.java
+++ ModuleDesign/src/GameEngine/Check/Checker.java
@@ -3,8 +3,5 @@ package GameEngine.Check;
 import java.util.HashMap;
 
 public interface Checker {
-    // boolean 
-    // public Integer countBulls(Integer num);
-    // public Integer countCows(Integer num);
     public HashMap<String, Integer> countBullsAndCows(Integer num1, Integer num2);
 }
