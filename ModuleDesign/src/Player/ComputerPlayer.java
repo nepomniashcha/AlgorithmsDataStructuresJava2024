2e7a6b46b82094d1043c2bc715baedacecd467be
nepomniashcha <marina.a.n555@gmail.com>
2024-11-18 12:48:12 +0200

    ю

--- ModuleDesign/src/Player/ComputerPlayer.java
+++ ModuleDesign/src/Player/ComputerPlayer.java
@@ -1,7 +1,13 @@
 package Player;
 
+import DataProcessing.Input.ConsoleInputData;
+
 public class ComputerPlayer implements NewPlayer {
+    private ArrayListInteger = new 
     public Integer makeMove() {
+        ConsoleInputData input = new ConsoleInputData();
+        input.readData();
+
 
         return 1;
     }
